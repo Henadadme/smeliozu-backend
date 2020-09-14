c732cd2ad57705cc2fd9f3a486fe6dd5b2bc8fac
mhero007 <momohhero2@gmail.com>
2020-09-14 22:30:26 +0100

    try config for typeorm production

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,13 +3,24 @@ import * as config from 'config';
 
 const dbConfig = config.get('db');
 
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: dbConfig.type,
+//   host: process.env.HOSTNAME || dbConfig.host,
+//   port: process.env.PORT || dbConfig.port,
+//   username: process.env.USERNAME || dbConfig.username,
+//   password: process.env.PASSWORD || dbConfig.password,
+//   database: process.env.DB_NAME || dbConfig.database,
+//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
+//   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
+// };
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  host: process.env.HOSTNAME || dbConfig.host,
-  port: process.env.PORT || dbConfig.port,
-  username: process.env.USERNAME || dbConfig.username,
-  password: process.env.PASSWORD || dbConfig.password,
-  database: process.env.DB_NAME || dbConfig.database,
+  host: dbConfig.host,
+  port: dbConfig.port,
+  username: dbConfig.username,
+  password: dbConfig.password,
+  database: dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
+  synchronize: dbConfig.synchronize,
 };
